ff993fcb4c82415ea69a80fa5a8db467c323aecf
Oleh-Liubimov <oleh.liubimov@gmail.com>
2024-05-10 11:40:50 +0300

    done

--- src/components/GalleryCard/GalleryCard.jsx
+++ src/components/GalleryCard/GalleryCard.jsx
@@ -4,7 +4,7 @@ export default function GalleryCard({ data: { urls, likes }, onImgClick }) {
     <div>
       <img
         src={urls.small}
-        className="w-96 h-52 rounded-lg block object-cover"
+        className="w-96 h-52 rounded-lg block object-cover hover:scale-105"
         onClick={() => onImgClick(urls.regular)}
       />
       <p className="text-center font-mono text-lg font-bold">Likes: {likes}</p>
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,20 +1,20 @@
 /* eslint-disable react/prop-types */
 import Modal from "react-modal";
-Modal.setAppElement("#root")
+Modal.setAppElement("#root");
 
 export default function ImageModal({ imgUrl, isOpen, onClose }) {
-    return (
-      <Modal
-        isOpen={isOpen}
-        shouldCloseOnEsc
-        shouldCloseOnOverlayClick
-        onRequestClose={onClose}
-        className="fixed inset-0 flex items-center justify-center"
-        overlayClassName="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-75"
-      >
-        <div className="bg-white rounded-lg p-4 max-w-full w-3/5 max-h-screen h-[95%] ">
-          <img src={imgUrl} className="mx-auto w-full h-full object-cover " />
-        </div>
-      </Modal>
-    );
+  return (
+    <Modal
+      isOpen={isOpen}
+      shouldCloseOnEsc
+      shouldCloseOnOverlayClick
+      onRequestClose={onClose}
+      className="fixed bg-white rounded-lg p-6 max-h-[95%] mx-auto"
+      overlayClassName="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-85"
+    >
+      <div className="flex justify-center items-center h-full">
+        <img src={imgUrl} className="max-w-full max-h-[80vh] object-contain" />
+      </div>
+    </Modal>
+  );
 }
